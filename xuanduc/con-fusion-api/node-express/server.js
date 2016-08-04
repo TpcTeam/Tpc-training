d6c0fd19d123c7f1a16830efecc811eb0f3bbe0a
xuanduc97uet <xuanduc1124@gmail.com>
2016-08-05 00:44:42 +0700

    conFusionApi 1

---
+++ xuanduc/con-fusion-api/node-express/server.js
@@ -0,0 +1,23 @@
+var dishRouter = require('./dishRouter');
+var promotionsRouter = require('./promotionsRouter');
+var leadershipRouter = require('./leadershipRouter');
+
+var express = require('express');
+var morgan = require('morgan');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use(morgan('dev'));
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promotionsRouter);
+app.use('/leadership', leadershipRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, hostname, function () {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
